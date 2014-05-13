0f47d24ade4cc2851ba77a4535575afbdc149635
Donagh Hatton <donaghhatton@gmail.com>
2014-05-13 22:09:11 +0100

    urls updated

--- nadb/urls.py
+++ nadb/urls.py
@@ -5,6 +5,6 @@ admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^shownotes/', include('shownotes.urls')),
+    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', include('shownotes.urls')),
 )
