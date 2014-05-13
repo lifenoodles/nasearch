189b0775a802edd300df875ad2926102b5ef0b3c
Donagh Hatton <donaghhatton@gmail.com>
2014-05-13 17:16:18 +0100

    models and gitignore updated

--- shownotes/models.py
+++ shownotes/models.py
@@ -5,16 +5,18 @@ class Show(models.Model):
     id = models.IntegerField(primary_key=True)
     name = models.TextField()
     date = models.DateField()
+    last_updated = models.DateTimeField()
 
     def __unicode__(self):
         return '{}: {}'.format(self.id, self.name)
 
 
 class Entry(models.Model):
-    fk_show = models.ForeignKey(Show)
+    show = models.ForeignKey(Show)
     topic = models.CharField(max_length=100)
     type = models.CharField(max_length=20)
     content = models.TextField()
 
     def __unicode__(self):
-        return '{} [{}]: {}'.format(self.topic, self.type, self.content[:20])
+        return '{} [{}]: {}'.format(
+            self.topic, self.type, self.content[:20])
