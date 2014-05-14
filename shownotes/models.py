be9eaef0489a63770efed2beb9fd46bd094a1efe
Donagh Hatton <donaghhatton@gmail.com>
2014-05-14 12:41:51 +0100

    model updated

--- shownotes/models.py
+++ shownotes/models.py
@@ -4,7 +4,6 @@ from django.db import models
 class Show(models.Model):
     id = models.IntegerField(primary_key=True)
     name = models.TextField()
-    date = models.DateField()
     last_updated = models.DateTimeField()
 
     def __unicode__(self):
@@ -15,6 +14,7 @@ class Entry(models.Model):
     show = models.ForeignKey(Show)
     topic = models.CharField(max_length=100)
     type = models.CharField(max_length=20)
+    title = models.TextField()
     content = models.TextField()
 
     def __unicode__(self):
