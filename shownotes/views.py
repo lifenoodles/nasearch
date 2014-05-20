99cf009b87853832a6090faf7f1990ed57e519a9
Donagh Hatton <donaghhatton@gmail.com>
2014-05-20 16:32:06 +0100

    view messing

--- shownotes/views.py
+++ shownotes/views.py
@@ -1,4 +1,4 @@
-from models import Note
+from models import Note, Show
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -10,9 +10,21 @@ def index(request):
 def search_topics(request):
     if 'string' in request.GET:
         string = request.GET['string']
-        notes = Note.get_by_topic(string)
-        titles = [note.title for note in notes]
-        context = {'titles': titles}
+        notes = Note.get_by_topic(string).order_by('-show')
+        shows = notes.values('show').distinct()
+        show_ids = [s['show'] for s in shows]
+        shows = Show.objects.in_bulk(show_ids)
+        shows = sorted(shows.values(),
+                       reverse=True,
+                       key=lambda x: x.id)
+        show_map = {}
+        for show in shows:
+            show_map[show] = []
+        for note in notes:
+            show_map[note.show].append(note)
+        print show_map
+
+        context = {'show_map': show_map, 'shows': shows}
         return render(
             request, 'shownotes/topic-list.html', context)
     else:
