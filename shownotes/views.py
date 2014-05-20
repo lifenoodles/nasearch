029eed615ea8542c5c2d3180ecd0968ee6ade886
Donagh Hatton <donaghhatton@gmail.com>
2014-05-20 20:43:34 +0100

    search view cleaned up

--- shownotes/views.py
+++ shownotes/views.py
@@ -11,13 +11,10 @@ def search_topics(request):
     if 'string' in request.GET:
         notes = Note.get_by_topic(
             request.GET['string']).order_by('-show')
-        shows = notes.values('show').distinct()
-        shows = Show.objects.in_bulk(
-            [s['show'] for s in shows]).values()
         show_map = defaultdict(list)
         for note in notes:
             show_map[note.show].append(note)
-        show_notes = [(show, show_map[show]) for show in shows]
+        show_notes = [(s, n) for s, n in show_map.items()]
         show_notes.sort(reverse=True,
                         key=lambda (x, _): x.id)
 
@@ -26,3 +23,7 @@ def search_topics(request):
             request, 'shownotes/topic-list.html', context)
     else:
         return render(request, 'shownotes/topic-list.html')
+
+
+def note_details(request):
+    pass
