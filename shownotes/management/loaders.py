2dc307b8dd0653f2327dcaf3c2d1ac876f07b2ce
Donagh Hatton <donaghhatton@gmail.com>
2014-05-20 12:37:14 +0100

    roll back opml loader changes

--- shownotes/management/loaders.py
+++ shownotes/management/loaders.py
@@ -18,20 +18,68 @@ class OpmlSourceError(Exception):
     pass
 
 
-class OpmlParser(object):
+class OpmlLoader(object):
     def __init__(self, source):
         self.source = source
-        self.show = Show()
-        self.notes = []
-        self.text_entries = []
-        self.url_entries = []
         try:
             self.data = opml.parse(source)
         except Exception as e:
             raise OpmlSourceError(e)
-        self._construct_show()
+        try:
+            self.number = self._show_number()
+            # we need an explicit copy here so we get the unicode str
+            self.title = self.data.title[:]
+        except ValueError:
+            raise ValueError('Bad opml data, no show number')
 
-    def construct_models(self):
+    def save(self):
+        if self.exists():
+            show = Show.objects.get(pk=self.number)
+            if self.date_modified() > show.last_updated:
+                show.delete()
+        if not self.exists():
+            self._insert_show()
+            self._insert_entries()
+
+    def date_modified(self):
+        try:
+            date_string = self.data.dateModified
+            return datetime.strptime(
+                date_string, '%a, %d %b %Y %H:%M:%S %Z')
+        except ValueError:
+            return datetime.fromtimestamp(0)
+
+    def exists(self):
+        return Show.objects.filter(pk=self.number).count() == 1
+
+    def _show_number(self):
+        numbers = self.data.title.split()
+        numbers = filter(lambda x: x.isdigit(), numbers)
+        if len(numbers) == 0:
+            raise ValueError
+        return int(numbers[0])
+
+    def _delete_show(self):
+        assert self.exists()
+        Show.objects.get(pk=self.number).delete()
+        assert not self.exists()
+
+    def _insert_show(self):
+        assert not self.exists()
+        show = Show(name=self.title, id=self.number,
+                    last_updated=self.date_modified())
+        show.save()
+        assert self.exists()
+
+    def _get_shownotes(self):
+        for v in self.data[1]:
+            if v.text == 'Shownotes':
+                return v
+        raise ValueError('No Shownotes Found')
+
+    def _insert_entries(self):
+        assert self.exists()
+        show = Show.objects.get(pk=self.number)
         shownotes = self._get_shownotes()
         for topic in shownotes:
             if len(topic) == 0:
@@ -41,9 +89,9 @@ class OpmlParser(object):
                 if len(note) == 0:
                     continue
                 new_note = Note(
-                    show=self.show, topic=topic_name,
+                    show=show, topic=topic_name,
                     title=note.text)
-                self.notes.append(new_note)
+                new_note.save()
                 full_text = []
                 for entry in note:
                     if hasattr(entry, 'type'):
@@ -51,65 +99,9 @@ class OpmlParser(object):
                             note=new_note,
                             text=entry.text,
                             url=entry.url)
-                        self.url_entries.append(new_entry)
+                        new_entry.save()
                     else:
                         full_text.append(entry.text)
                 new_entry = TextEntry(
                     note=new_note, text='\n'.join(full_text))
-                self.text_entries.append(new_entry)
-
-    def _construct_show(self):
-        try:
-            # we need an explicit copy here so we get the unicode str
-            self.show = Show(
-                id=self._show_number(),
-                name=self.data.title[:],
-                last_updated=self._date_modified())
-        except ValueError:
-            raise ValueError('Bad opml data, no show number')
-
-    def _date_modified(self):
-        try:
-            date_string = self.data.dateModified
-            return datetime.strptime(
-                date_string, '%a, %d %b %Y %H:%M:%S %Z')
-        except ValueError:
-            return datetime.fromtimestamp(0)
-
-    def _show_number(self):
-        numbers = self.data.title.split()
-        numbers = filter(lambda x: x.isdigit(), numbers)
-        if len(numbers) == 0:
-            raise ValueError
-        return int(numbers[0])
-
-    def _get_shownotes(self):
-        for v in self.data[1]:
-            if v.text == 'Shownotes':
-                return v
-        raise ValueError('No Shownotes Found')
-
-
-class OpmlLoader(object):
-    def __init__(self, source):
-        self.parser = OpmlParser(source)
-        self.show_id = self.parser.show.id
-
-    def save(self):
-        if Show.objects.filter(id=self.parser.show.id).exists():
-            old_show = Show.objects.get(pk=self.parser.show.id)
-            if self.parser.show.last_updated > old_show.last_updated:
-                show.delete()
-        if not Show.objects.filter(id=self.parser.show.id).exists():
-            self.parser.construct_models()
-            self.parser.show.save()
-            # print repr(self.parser.show)
-            for note in self.parser.notes:
-                note.save()
-                # print note
-            for entry in self.parser.url_entries:
-                entry.save()
-                # print entry
-            for entry in self.parser.text_entries:
-                entry.save()
-                # print entry
+                new_entry.save()
