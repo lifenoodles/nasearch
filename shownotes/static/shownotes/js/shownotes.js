3f646e6e5e213ea4cfa02c12e4512f51e20ea210
Donagh Hatton <donaghhatton@gmail.com>
2014-05-22 01:02:38 +0100

    adding/removing topics inital support

--- shownotes/static/shownotes/js/shownotes.js
+++ shownotes/static/shownotes/js/shownotes.js
@@ -5,30 +5,78 @@
     "use strict";
 
     function TopicPopupView() {
-        this.template = $('#topic-list-template').html();
+        var self = this;
+        self.template = $('#topic-list-template').html();
+        self.selectedTemplate = $('#topic-selected-template').html();
+        self.view = {};
+        self.topics = {};
 
-        this.render = function (view) {
-            var rendered = Mustache.render(this.template, view);
-            $('#topic-field').after(rendered);
+        self.click_unpicked = function () {
+            var text = $(this).html(),
+                topic = self.topics[text],
+                id = topic.id;
+            // topic.on = false;
+            $(this).hide();
+            $(".selected-topics").find("#topic" + id).toggle();
+            console.log($(".selected-topics").find("#topic" + id).length);
         };
 
-        this.getTopics = function () {
+        self.click_picked = function () {
+            var text = $(this).html(),
+                topic = self.topics[text],
+                id = topic.id;
+            // topic.on = true;
+            $(this).hide();
+            $(".topic-suggestions").find("#topic" + id).toggle();
+        };
+
+        self.render = function () {
+            $('#topic-field').after(
+                Mustache.render(self.template, self.view)
+            );
+            $('.selected-topics').append(
+                Mustache.render(self.selectedTemplate, self.view)
+            );
+        };
+
+        self.getTopics = function () {
+            var i,
+                topics = [
+                    {text: "topic1", on: true, id: 1},
+                    {text: "topic2", on: true, id: 2},
+                    {text: "topic3", on: true, id: 3},
+                    {text: "topic4", on: true, id: 4},
+                    {text: "topic5", on: true, id: 5}];
             //TODO: do ajax, fake it for now
-            var view = {
-                topics: ["topic1", "topic2", "topic3",
-                    "topic4", "topic5"]
+            self.view = {
+                topics: topics
             };
-            this.render(view);
+
+            for (i = 0; i < topics.length; i += 1) {
+                self.topics[topics[i].text] = topics[i];
+            }
+            // self.topics.push.apply(topics);
+
+            // attach event handlers to topic objects
+            self.render();
+            $(".topic-suggestions").find(".topic-suggestion")
+                .click(self.click_unpicked);
+            $(".selected-topics").find(".topic-suggestion")
+                .click(self.click_picked);
         };
 
-        this.filter = function () {
+        self.pick = function () {
+            return null;
+        };
+
+        self.unpick = function () {
             return null;
         };
 
         // register event handlers
 
         // initialise data and render
-        this.getTopics();
+        self.getTopics();
     }
 
     $(document).ready(function () {
