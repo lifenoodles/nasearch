75a792ffe074da7e369f993f1787638d55c060f0
Donagh Hatton <donaghhatton@gmail.com>
2014-06-14 01:45:17 +0100

    new functions added to netutils

--- common/netutils.py
+++ common/netutils.py
@@ -1,6 +1,14 @@
 import requests
 import feedparser
 from bs4 import BeautifulSoup
+import re
+
+
+def get_html(url):
+    response = requests.get(url, timeout=30)
+    if response.status_code == 200:
+        return response.text
+    raise IOError('No text received')
 
 
 def get_pages(*urls):
@@ -25,6 +33,15 @@ def extract_urls_from_rss(feed):
     returns a list of urls linked to by this feed
     '''
     return [x['href'] for entry in feed.entries for x in entry.links]
+
+
+def get_links_to(pattern, html):
+    '''
+    return a list of links that match the given pattern
+    '''
+    soup = BeautifulSoup(html)
+    links = [x['href'] for x in soup.find_all('a', href=True)]
+    links = [l for l in links if re.search(pattern, l.text)]
 
 
 def extract_urls_from_html(html):
