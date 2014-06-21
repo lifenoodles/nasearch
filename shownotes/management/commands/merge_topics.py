c60ccf5e39ec4212b9aa12f3dda1d6fecfdfbf2a
Donagh Hatton <donaghhatton@gmail.com>
2014-06-21 17:06:07 +0100

    merge doing cleanup

--- shownotes/management/commands/merge_topics.py
+++ shownotes/management/commands/merge_topics.py
@@ -1,21 +1,30 @@
 from shownotes.models import Topic
 from django.core.management.base import BaseCommand
+from django.utils.html import strip_tags
 
 
 class Command(BaseCommand):
     help = '''Merges topics that are meant to be the same
               prior to index building'''
-    TOPIC_LISTS = [
-        (['99 Fists', '99Fists'], '99 Fists'),
-        (['99 Fists', '99Fists'], '99 Fists')
+    BAD_TOPICS = [
+        '<a href="http://search.nashownotes.com">Search</a>'
     ]
 
-    def clean_empty_topics():
-        unused_topics = [t for t in Topic.all() if t.note_count() == 0]
+    def delete_empty_topics(self):
+        unused_topics = [t for t in Topic.objects.all()
+                         if t.note_count() == 0]
         for topic in unused_topics:
             topic.delete()
 
+    def delete_bad_topics(self):
+        for topic in Topic.objects.filter(name__in=Command.BAD_TOPICS):
+            topic.delete()
+
+    def clean_topic_names(self):
+        for topic in Topic.objects.all():
+            topic.name = strip_tags(topic.name)
+            topic.save()
+
     def handle(self, *args, **options):
-        self.clean_empty_topics()
-        for tlist, name in Command.TOPIC_LISTS:
-            pass
+        self.delete_empty_topics()
+        self.delete_bad_topics()
