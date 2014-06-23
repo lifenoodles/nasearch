9719d62c5ee73c976dc40cfd6ed5f5a296613946
Donagh Hatton <donaghhatton@gmail.com>
2014-06-23 10:59:50 +0100

    merge_topics renamed to clean_topics

--- shownotes/management/commands/clean_topics.py
+++ shownotes/management/commands/clean_topics.py
@@ -1,14 +1,11 @@
 from shownotes.models import Topic
 from django.core.management.base import BaseCommand
-from django.utils.html import strip_tags
 
 
 class Command(BaseCommand):
     help = '''Merges topics that are meant to be the same
               prior to index building'''
-    BAD_TOPICS = [
-        '<a href="http://search.nashownotes.com">Search</a>'
-    ]
+    BAD_TOPICS = []
 
     def delete_empty_topics(self):
         unused_topics = [t for t in Topic.objects.all()
@@ -19,11 +16,6 @@ class Command(BaseCommand):
     def delete_bad_topics(self):
         for topic in Topic.objects.filter(name__in=Command.BAD_TOPICS):
             topic.delete()
-
-    def clean_topic_names(self):
-        for topic in Topic.objects.all():
-            topic.name = strip_tags(topic.name)
-            topic.save()
 
     def handle(self, *args, **options):
         self.delete_empty_topics()
