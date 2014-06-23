e92f0fc6eddbf397dea109a6936f4429e1aba8fb
Donagh Hatton <donaghhatton@gmail.com>
2014-06-23 11:31:22 +0100

    scripts updated

---
+++ fetch_updates.sh
@@ -0,0 +1,4 @@
+source ../bin/activate
+python manage.py rss_update
+python manage.py clean_topics
+python manage.py update_index --age 1
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,12 +8,12 @@ rsync -rv --relative -e "ssh -i /home/donagh/.ssh/server.pem" \
 --exclude "settings_dev.py" \
 --exclude ".git" \
 --exclude ".gitignore" \
---exclude "deploy.sh" \
 --exclude "*.opml" \
 --exclude "*.fuse*" \
 --exclude "*.sublime-*" \
 --exclude "scripts/" \
 --exclude "deploy/" \
+--exclude "todo.txt" \
 . ubuntu@54.213.153.244:~/venv/noagenda-db/noagenda-db/
 
 ssh -i ~/.ssh/server.pem ubuntu@54.213.153.244 "source ~/venv/noagenda-db/bin/activate;cd ~/venv/noagenda-db/noagenda-db;python manage.py collectstatic --noinput"
