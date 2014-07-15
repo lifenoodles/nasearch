a1108dd036d38e31aab4607909fb3caf191c6880
Donagh Hatton <donaghhatton@gmail.com>
2014-07-15 13:27:53 +0100

    removed docker files and reorganised configs

--- scratch.py
+++
@@ -1,6 +0,0 @@
-from shownotes.management.loaders import HtmlLoader
-import requests
-
-response = requests.get('http://static.curry.com/nashownotes/554/index.html')
-loader = HtmlLoader(response.text, 554)
-loader.save()
