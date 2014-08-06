d5ab01cb655e9e2241f9586900e99f80d1e9c745
Donagh Hatton <donaghhatton@gmail.com>
2014-08-06 12:59:31 +0100

    updated url patterns to include api patterns

--- shownotes/urls.py
+++ shownotes/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, url
 from shownotes import views
+from shownotes import api
 
 urlpatterns = patterns(
     '',
     url(r'^$', views.index),
     url(r'^search$', views.search_topics),
-    url(r'^topics$', views.topics)
+    url(r'^topics$', views.topics),
+    url(r'^api/topics$', api.topics)
 )
