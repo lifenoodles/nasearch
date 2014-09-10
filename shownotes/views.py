211ecf6aeaa2474eb9d36068269e4021af152907
Donagh Hatton <donaghhatton@gmail.com>
2014-09-10 14:34:50 +0100

    searches refactored to use internal methods

--- shownotes/views.py
+++ shownotes/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render
 from django.template.loader import render_to_string
 from django.http import HttpResponse
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.db.models import Max, Min
-from haystack.query import SearchQuerySet, SQ
-from haystack.inputs import AutoQuery
 from shownotes.models import Show
 import json
+import searches
 
 
 TOPIC_SEARCH_LIMIT = 10
@@ -16,65 +14,50 @@ RESULTS_SEARCH_LIMIT = 20
 def index(request):
     agg = Show.objects.all().aggregate(Max('id'), Min('id'))
     context = {'show_numbers':
-               [x for x in range(agg['id__min'], agg['id__max'])]}
+               [x for x in range(agg['id__min'], agg['id__max'] + 1)]}
     print context
     return render(request, 'shownotes/index.html', context)
 
 
 def search_topics(request):
-    if 'string' in request.GET \
-            and 'topics' in request.GET \
-            and 'page' in request.GET:
-        results = []
-        topic_ids = [int(t) for t in request.GET['topics'].split()]
-        context = {}
-        response_dict = {'page': 0, 'page_count': 0}
+    """
+    perform a search and return the matches
+    """
+    topics = []
+    if 'topics' in request.GET:
+        topics = [int(t) for t in request.GET['topics'].split() if t.isdigit()]
+    string = ''
+    if 'string' in request.GET:
+        string = request.GET['string']
+    min_show = 0
+    if 'min_show' in request.GET and request.GET['min_show'].isdigit():
+        min_show = int(request.GET['min_show'])
+    max_show = None
+    if 'max_show' in request.GET and request.GET['max_show'].isdigit():
+        max_show = int(request.GET['max_show'])
+    page = 1
+    if 'page' in request.GET and request.GET['page'].isdigit():
+        page = int(request.GET['page'])
 
-        if len(topic_ids) > TOPIC_SEARCH_LIMIT:
-            context['limit'] = TOPIC_SEARCH_LIMIT
-            topic_ids = topic_ids[:TOPIC_SEARCH_LIMIT]
-
-        if request.GET['string'] == '':
-            if len(topic_ids) == 0:
-                response_dict['html'] = render_to_string(
-                    'shownotes/empty-topic.html')
-                return HttpResponse(json.dumps(response_dict),
-                                    content_type='application/json')
-
-            results = SearchQuerySet().filter(topic_id__in=topic_ids) \
-                .order_by('show_number').order_by('topic_name')
-        else:
-            query = request.GET['string']
-            print topic_ids
-            results = SearchQuerySet().filter_and(
-                SQ(text=AutoQuery(query)), topic_id__in=topic_ids)
-
-        context['matches'] = len(results)
-        paginator = Paginator(results, RESULTS_SEARCH_LIMIT)
-        response_dict['page_count'] = paginator.num_pages
-        try:
-            results = paginator.page(request.GET['page'])
-            response_dict['page'] = int(request.GET['page'])
-        except PageNotAnInteger:
-            results = paginator.page(1)
-            response_dict['page'] = 1
-        except EmptyPage:
-            results = []
-            response_dict['page'] = paginator.num_pages
-
-        context['results'] = results
-        if request.GET['page'] == '1':
-            response_dict['html'] = render_to_string(
-                'shownotes/topic-container.html', context)
-        else:
-            response_dict['html'] = render_to_string(
-                'shownotes/topic-list.html', context)
-        return HttpResponse(json.dumps(response_dict),
+    response = {'html': '', 'page': 1, 'page_count': 1}
+    if string == '' and topics == []:
+        response['html'] = render_to_string('shownotes/empty-topic.html')
+        return HttpResponse(json.dumps(response),
                             content_type='application/json')
-    else:
-        html = render_to_string(
-            'shownotes/topic-container.html')
-        return HttpResponse(json.dumps({'page': '0',
-                                        'page_count': '0',
-                                        'html': html}),
-                            content_type='application/json')
+
+    context = {}
+    if len(topics) > TOPIC_SEARCH_LIMIT:
+        context['limit'] = TOPIC_SEARCH_LIMIT
+        topics = topics[:TOPIC_SEARCH_LIMIT]
+
+    results = searches.search(string, topics, min_show, max_show)
+    context['matches'] = results.count()
+    context['results'], response['page'], response['page_count'] = \
+        searches.paginate(results, page, RESULTS_SEARCH_LIMIT)
+
+    template = 'shownotes/topic-container.html'
+    if page > 1:
+        template = 'shownotes/topic-list.html'
+    response['html'] = render_to_string(template, context)
+    return HttpResponse(json.dumps(response),
+                        content_type='application/json')
